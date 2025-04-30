d89417337629e808ad7dd54a30e5d57b77c0bebc
bynzo <78451854+bynzo@users.noreply.github.com>
2025-04-30 09:49:46 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,34 @@
 self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('book-scanner-v1').then(cache =>
-            cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './icon-192.png',
-                './icon-512.png'
-                // DO NOT add external URLs here
-            ])
-        ).catch(err => console.error("Cache install error:", err))
-    );
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open('book-scanner-v2').then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './styles.css',
+        './app.js',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png',
+        './sw.js'
+      ])
+    )
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== 'book-scanner-v2').map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request)
+      .then(resp => resp || fetch(e.request))
+  );
 });
